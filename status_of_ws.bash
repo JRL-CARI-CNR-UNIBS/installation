0025d481365b19cd19a0db1eb9aee0881016e00f
Robothlon <robothlon@galois.cari>
2021-06-16 16:25:09 +0200

    add status of ws

---
+++ status_of_ws.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+
+wstool status -t ~/projects/third_parties_ws/src
+wstool status -t ~/projects/control_ws/src
+wstool status -t ~/projects/planning_ws/src
+wstool status -t ~/projects/perception_ws/src
+wstool status -t ~/projects/sharework_ws/src
