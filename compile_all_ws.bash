436317ea222da5fc43aeafc8e4f70214b1521ea8
Manuel Beschi <manuel.beschi@unibs.it>
2021-08-12 14:44:37 +0200

    add compite all ws

--- compile_all_ws.bash
+++ compile_all_ws.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-catkin build -cs --workspace ~/projects/third_parties_ws/
-catkin build -cs --workspace ~/projects/control_ws/
-catkin build -cs --workspace ~/projects/planning_ws/
-catkin build -cs --workspace ~/projects/perception_ws/
-catkin build -cs --workspace ~/projects/sharework_ws/
+catkin build -cs --workspace ~/projects/third_parties_ws/  --mem-limit 30%
+catkin build -cs --workspace ~/projects/control_ws/  --mem-limit 30%
+catkin build -cs --workspace ~/projects/planning_ws/  --mem-limit 30%
+catkin build -cs --workspace ~/projects/perception_ws/  --mem-limit 30%
+catkin build -cs --workspace ~/projects/sharework_ws/  --mem-limit 30%
