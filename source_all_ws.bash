6c7d77d0000b43995eb5dfd0d7ef8da50b10515e
Marco Faroni <marco.faroni@stiima.cnr.it>
2022-03-23 19:38:32 +0100

    improve compile_all_ws

--- source_all_ws.bash
+++ source_all_ws.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-
-
 source ~/projects/third_parties_ws/devel/setup.bash
 source ~/projects/control_ws/devel/setup.bash
 source ~/projects/planning_ws/devel/setup.bash
 source ~/projects/perception_ws/devel/setup.bash
 source ~/projects/sharework_ws/devel/setup.bash
+source ~/projects/cells_ws/devel/setup.bash
