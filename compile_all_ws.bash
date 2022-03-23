6752aef2b61cdc9412ab9963163c8d04cfc10692
Marco Faroni <marco.faroni@stiima.cnr.it>
2022-03-23 19:41:03 +0100

    improve compile_all_ws

--- compile_all_ws.bash
+++ compile_all_ws.bash
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-ROOT_DIR=/home/hypatia/projects
-
 ###
 source /opt/ros/noetic/setup.bash
 
 ###
-catkin build -cs --workspace $ROOT_DIR/third_parties_ws/  --mem-limit 30% -j 4
-source $ROOT_DIR/third_parties_ws/devel/setup.bash
+catkin build -cs --workspace ~/projects/third_parties_ws/  --mem-limit 30% -j 4
+source ~/projects/third_parties_ws/devel/setup.bash
 
 ###
-catkin build -cs --workspace $ROOT_DIR/control_ws/  --mem-limit 30% -j 4
-source $ROOT_DIR/control_ws/devel/setup.bash
+catkin build -cs --workspace ~/projects/control_ws/  --mem-limit 30% -j 4
+source ~/projects/control_ws/devel/setup.bash
 
 ###
-catkin build -cs --workspace $ROOT_DIR/planning_ws/  --mem-limit 30% -j 4
-source $ROOT_DIR/planning_ws/devel/setup.bash
+catkin build -cs --workspace ~/projects/planning_ws/  --mem-limit 30% -j 4
+source ~/projects/planning_ws/devel/setup.bash
 
 ###
-catkin build -cs --workspace $ROOT_DIR/perception_ws/  --mem-limit 30% -j 4
-source $ROOT_DIR/perception_ws/devel/setup.bash
+catkin build -cs --workspace ~/projects/perception_ws/  --mem-limit 30% -j 4
+source ~/projects/perception_ws/devel/setup.bash
 
 ###
-catkin build -cs --workspace $ROOT_DIR/sharework_ws/  --mem-limit 30% -j 4
-source $ROOT_DIR/sharework_ws/devel/setup.bash
+catkin build -cs --workspace ~/projects/sharework_ws/  --mem-limit 30% -j 4
+source ~/projects/sharework_ws/devel/setup.bash
