1a0ec9a84a15eb9490500e61a6657062dc679a32
MarcoFaroni <marco.faroni@stiima.cnr.it>
2022-08-26 14:40:35 -0400

    marco: um settings

--- status_of_ws.bash
+++ status_of_ws.bash
@@ -2,8 +2,4 @@
 
 
 
-wstool status -t ~/projects/third_parties_ws/src
-wstool status -t ~/projects/control_ws/src
-wstool status -t ~/projects/planning_ws/src
-wstool status -t ~/projects/perception_ws/src
-wstool status -t ~/projects/sharework_ws/src
+wstool status -t ~/projects/arm_robots/src
--- update_all_ws.bash
+++ update_all_ws.bash
@@ -2,8 +2,4 @@
 
 
 
-wstool update -t ~/projects/third_parties_ws/src
-wstool update -t ~/projects/control_ws/src
-wstool update -t ~/projects/planning_ws/src
-wstool update -t ~/projects/perception_ws/src
-wstool update -t ~/projects/sharework_ws/src
+wstool update -t ~/projects/arm_robots/src
